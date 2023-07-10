7fe6139cdab47a4a420c0eb2f14234a0bfe3f229
Vasilii Iurkin <vasiliiiurkin@MacBook-Air-Vasilii.local>
2023-07-10 22:08:28 +0300

    added absolute imports and alias

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import AboutPageAsync from "./pages/AboutPage/AboutPageAsync";
-import MainPageAsync from "./pages/MainPage/MainPageAsync";
 import { Suspense } from "react";
-import useTheme from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { classNames } from "@/shared/lib/classNames/classNames";
+import { useTheme } from "@/app/providers/ThemeProvider";
+import { AboutPage } from "@/pages/AboutPage";
+import { MainPage } from "@/pages/MainPage";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,8 +16,8 @@ const App = () => {
       <Link to="/about">About Page</Link>
       <Suspense fallback={<div>Loading ...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
